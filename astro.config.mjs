167a99440090a06822631e951b288985fb987b20
Mirza Pandzo <m_pandzo@yahoo.com.au>
2023-08-14 17:54:57 +1000

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import mdx from "@astrojs/mdx";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://indevitus.com',
+  integrations: [tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false
+  }), sitemap(), robotsTxt(), mdx(), alpinejs()],
+  trailingSlash: 'never'
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'sans': ['Work Sans', 'Arial', 'sans-serif'],
+				'display': ['Poppins', 'Arial', 'sans-serif'],
+			},			
+			container: {
+				center: true,
+			},
+			colors: {
+				'gold': '#c6932a',
+			},
+			screens: {
+				'2xl': '1280px',
+				'3xl': '1280px',
+			}				
+		},
+	},
+	plugins: [],
+}
