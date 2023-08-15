89f27736ff94de3cf5f468b08fcc5f0f4e65b27e
Mirza Pandzo <m_pandzo@yahoo.com.au>
2023-08-15 14:13:00 +1000

    Added a simple reload

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,47 @@
 import Glide from '@glidejs/glide';
 
 (function() {
+
+    const traverseAndAddClass = (parentClass, childClass) => {
+        if (document.querySelectorAll(parentClass) && document.querySelectorAll(parentClass).length > 0) {
+            document.querySelectorAll(parentClass).forEach(element => {
+                element.classList.add(childClass); 
+            });        
+        }
+    }
+
+    const traverseAndRemoveClass = (parentClass, childClass) => {
+        if (document.querySelectorAll(parentClass) && document.querySelectorAll(parentClass).length > 0) {
+            document.querySelectorAll(parentClass).forEach(element => {
+                element.classList.remove(childClass); 
+            });        
+        }
+    }    
+
     if (window.isMobile()) {
         if (document.querySelector('.glide-team') || document.querySelector('.glide-skills')) {
-            document.querySelectorAll('.__glide').forEach(element => {
-                element.classList.add('glide'); 
-            });
-            document.querySelectorAll('.__glide__track').forEach(element => {
-                element.classList.add('glide__track'); 
-            });
-            document.querySelectorAll('.__glide__bullets').forEach(element => {
-                element.classList.add('glide__bullets'); 
-            });
-            document.querySelectorAll('.__glide__slides').forEach(element => {
-                element.classList.add('glide__slides'); 
-            });                        
+            traverseAndAddClass('.__glide', 'glide');
+            traverseAndAddClass('.__glide__track', 'glide__track');
+            traverseAndAddClass('.__glide__bullets', 'glide__bullets');
+            traverseAndAddClass('.__glide__bullet', 'glide__bullet');
+            traverseAndAddClass('.__glide__slides', 'glide__slides');
+            traverseAndAddClass('.__glide__slide', 'glide__slide');
 
             document.querySelectorAll('.__glide__slide.hidden').forEach(element => {
                 element.remove();
             });
-            document.querySelectorAll('.__glide__slide').forEach(element => {
-               element.classList.add('glide__slide'); 
-            });
-            document.querySelectorAll('.__glide__bullet').forEach(element => {
-               element.classList.add('glide__bullet'); 
-            });
         }
 
+        let teamGlider, skillsGlider;
         if (document.querySelector('.glide-team')) {
-            new Glide('.glide-team', { autoplay: 2000, gap: 0 }).mount();
+            teamGlider = new Glide('.glide-team', { autoplay: 2000, gap: 0 }).mount();
         }
         if (document.querySelector('.glide-skills')) {
-            new Glide('.glide-skills', { autoplay: 3000, gap: 0 }).mount();
+            skillsGlider = new Glide('.glide-skills', { autoplay: 3000, gap: 0 }).mount();
         }
-
-        window.addEventListener("resize", () => {
-            
-        });
     }
+
+    window.addEventListener('resize', () => {
+        location.reload();
+    }, true);       
  })();
