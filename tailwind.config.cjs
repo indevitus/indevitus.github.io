13eb44bb47788b58657acb7f91e2270ea14303be
Meshakarry <mehmed.karadza@edu.fit.ba>
2024-04-15 17:10:49 +0200

    fix: mobile menu, duplicated code, logo, fonts loading, menu links, header styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,18 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			// 'sans': ['Work Sans', 'Arial', 'sans-serif'],
+			'display': ['Poppins', 'sans-serif']
+		},
 		extend: {
-			fontFamily: {
-				'sans': ['Work Sans', 'Arial', 'sans-serif'],
-				'display': ['Poppins', 'Arial', 'sans-serif'],
-			},			
+			// fontFamily: {
+			// 	'sans': ['Work Sans', 'Arial', 'sans-serif'],
+			// 	'display': ['Poppins', 'sans-serif'],
+			// },
 			container: {
 				center: true,
+				padding: '1rem'
 			},
 			colors: {
 				'gold': '#c6932a',
@@ -21,7 +26,7 @@ module.exports = {
 			screens: {
 				'2xl': '1280px',
 				'3xl': '1280px',
-			}				
+			}
 		},
 	},
 	plugins: [],
