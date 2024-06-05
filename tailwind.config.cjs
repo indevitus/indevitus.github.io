43cdd6f479f4f202fc03e9490a8d76e62291420e
OHusan <omerinovic27@gmail.com>
2024-06-05 16:49:40 +0200

    add: optimized tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
 		},
 		extend: {
 			fontFamily: {
-				sans: ['Poppins', 'sans-serif'],
+				sans: ['Poppins', ...defaultTheme.fontFamily.sans],
 				archivoBlack: ['Archivo Black', 'Poppins', 'sans-serif']
 			},
 			container: {
