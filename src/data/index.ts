017408ebd212d9a6eef9fed41a910dac622eb0a4
OHusan <omerinovic27@gmail.com>
2024-06-21 15:38:03 +0200

    add: update team component, team data, employee image

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,3 @@
-import { JOB_DESCRIPTION } from "./jobs";
-import { CLIENT_LIST } from "./clients";
-
-export {JOB_DESCRIPTION, CLIENT_LIST}
+export * from "./jobs"
+export * from "./clients"
+export * from "./team"
