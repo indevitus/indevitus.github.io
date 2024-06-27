2c6bc71827e671a4c3601c79256ab42ff49a90f1
OHusan <omerinovic27@gmail.com>
2024-06-27 15:13:47 +0200

    fix: lowercase names

--- src/data/team.ts
+++ src/data/team.ts
@@ -2,63 +2,63 @@ export const TEAM_INFO = [
   {
     id: 1,
     full_name: 'Adnan Rizvan',
-    name: 'adnan',
+    name: 'Adnan',
     position: 'Senior Developer',
     linkedIn: 'https://www.linkedin.com/in/rizvanadnan/'
   },
   {
     id: 2,
     full_name: 'Haris Begić',
-    name: 'haris',
+    name: 'Haris',
     position: 'Software Engineer',
     linkedIn: 'https://www.linkedin.com/in/haris-begi%C4%87-2b73531a9/'
   },
   {
     id: 3,
     full_name: 'Aldin Mikara',
-    name: 'aldin',
+    name: 'Aldin',
     position: 'Frontend Developer',
     linkedIn: 'https://www.linkedin.com/in/aldin-mikara-08b9a7201/'
   },
   {
     id: 4,
     full_name: 'Harun Mešan',
-    name: 'harun',
+    name: 'Harun',
     position: 'Frontend Developer',
     linkedIn: 'https://www.linkedin.com/in/harun-me%C5%A1an-34b535227/'
   },
   {
     id: 5,
     full_name: 'Mehmed Karadža',
-    name: 'mehmed',
+    name: 'Mehmed',
     position: 'Frontend Developer',
     linkedIn: 'https://www.linkedin.com/in/mehmedkaradza'
   },
   {
     id: 6,
     full_name: 'Emir Gajip',
-    name: 'emir',
+    name: 'Emir',
     position: 'Frontend Developer',
     linkedIn: 'https://www.linkedin.com/in/emir-gajip-b2979a202/'
   },
   {
     id: 7,
     full_name: 'Nejra Duvnjak',
-    name:'nejra',
+    name:'Nejra',
     position: 'Junior Frontend Developer',
     linkedIn: 'https://www.linkedin.com/in/nejra-duvnjak-61296b233/'
   },
   {
     id: 8,
     full_name: 'Dino Halilović',
-    name:'dino',
+    name:'Dino',
     position: 'Frontend Developer',
     linkedIn: 'https://www.linkedin.com/in/dino-halke/'
   },
   {
     id: 9,
     full_name: 'Husan Omerinović',
-    name: 'husan',
+    name: 'Husan',
     position: 'Junior Frontend Developer',
     linkedIn: 'https://www.linkedin.com/in/husan-omerinovic-69b5b8286/'
   }
